69f1909e056e16d6df66d7dde228243f60160a03
Omar Badr <omarbadr938@gmail.com>
2023-10-30 14:44:38 +0100

    update index.html

--- D2.js
+++ D2.js
@@ -1,3 +1,5 @@
 const fahrenheitToCelsius = fahrenheit => (fahrenheit - 32) * 5 / 9;
 
 console.log(fahrenheitToCelsius(15));
+console.log(fahrenheitToCelsius(59));
+console.log(fahrenheitToCelsius(72));
